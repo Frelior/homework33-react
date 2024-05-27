c0313b6f9a74e4ad8ca29401716d8e3adc6d389b
Rudenko Mykhailo <fernandosolotop@gmail.com>
2024-05-27 14:49:03 +0300

    changed colors

--- src/homework38/HomeWork38/HomeWork38.js
+++ src/homework38/HomeWork38/HomeWork38.js
@@ -1,7 +1,7 @@
 import { styled } from "styled-components"
 
 export const StyledHomeWork35 = styled.div`
-  background-color: #ffdede;
+  background-color: #fffcde;
   width: 100%;
   height: 100%;
   max-height: 100%;
